d84809f6ddee7e6e8d3971e462385a279d0e7f21
Nava <cyberjosedaniel@gmail.com>
2024-07-01 23:47:31 -0500

    my first component, and insert word "Hola"

---
+++ src/components/MyFirstComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const MyFirstComponent = () => {
+  return (
+    <div>
+        <div>MyFirstComponent</div>
+        <div>Hello world, I'm Jose Navarrete</div>    
+        <div>27 de junio 2024</div>    
+    </div>
+  )
+}
+
